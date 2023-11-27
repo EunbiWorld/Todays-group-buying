1de9d88cafe30b1b9aca5e5191937a2676058e14
sehoon0302 <saehun66@gmail.com>
2023-11-27 22:48:15 +0900

    세팅 페이지 디자인 완성

--- app/src/main/java/com/example/groupbuying/fragment/SettingFragment.java
+++ app/src/main/java/com/example/groupbuying/fragment/SettingFragment.java
@@ -1,8 +1,23 @@
 package com.example.groupbuying.fragment;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.example.groupbuying.R;
+
 public class SettingFragment extends Fragment {
+    @NonNull
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState){
+        View view = inflater.inflate(R.layout.fragment_setting, container, false);
+        return view;
+    }
 
 
 }
